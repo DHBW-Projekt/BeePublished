61b2a7887e03af92e0aeefdb37d23953e6eaef39
Christoph Kraemer <mail@christophkraemer.de>
2011-12-06 12:19:52 +0100

    Added Sample Plugins

---
+++ plugins/Guestbook/Model/GuestbookEntry.php
@@ -0,0 +1,35 @@
+<?php
+App::uses('GuestbookAppModel', 'Guestbook.Model');
+/**
+ * GuestbookEntry Model
+ *
+ */
+class GuestbookEntry extends GuestbookAppModel {
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'email' => array(
+			'email' => array(
+				'rule' => array('email'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
