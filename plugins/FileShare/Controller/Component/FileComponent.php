2840d07f017eaa46d6647521f3ad42393aae95ee
Peter Baumert <peter.baumert@gmx.info>
2011-12-19 14:56:36 +0100

    FileShare Plugin erstellt

---
+++ plugins/FileShare/Controller/Component/FileComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+class FileComponent extends Component
+{
+    public function upload()
+    {
+        if (!empty($this->data) &&
+            is_uploaded_file($this->data['MyFile']['File']['tmp_name'])
+        ) {
+            //$fileData = fread(fopen($this->data['File']['File']['tmp_name'], "r"),$this->data['File']['File']['size']);
+
+            $this->data['MyFile']['filename'] = $this->data['MyFile']['File']['name'];
+            $this->data['MyFile']['type'] = $this->data['MyFile']['File']['type'];
+            $this->data['MyFile']['size'] = $this->data['MyFile']['File']['size'];
+            // Write file to disk
+            //$this->data['File']['data'] = $fileData;
+
+            $this->data['MyFile']['path'] = "muh";
+            $this->data['MyFile']['user'] = "1";
+
+            $this->MyFile->save($this->data);
+
+            //$this->redirect('somecontroller/someaction');
+        }
+    }
+        public function beforeFilter() {
+        parent::beforeFilter();
+
+        //Actions which don't require authorization
+        $this->Auth->allow('*');
+    }
+}
+
+?>
---
+++ plugins/FileShare/Model/MyFile.php
@@ -0,0 +1,88 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * File Model
+ *
+ */
+class MyFile extends AppModel {
+
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'ID';
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'filename';
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'ID' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'filename' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'type' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'size' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'path' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'owner' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
