ecee85bd6a49ca2052b5303e6ef5e23b4bf587ce
SebastianHaase <sebastian.x@web.de>
2011-12-20 22:04:55 +0100

    Missing: - Auth - Design - When to show what paging information

--- plugins/Guestbook/Controller/Component/DisplayComponent.php
+++ plugins/Guestbook/Controller/Component/DisplayComponent.php
@@ -1,19 +1,37 @@
 <?php
 
 class DisplayComponent extends Component {
-	
-	public $components = array('Guestbook.Guestbook');
-	
-	public function getData($controller, $params)
+
+	public $name = 'GuestbookComponent';
+	public $components = array('Paginator');
+
+	function beforeFilter()
 	{
-		return $this->Guestbook->display($controller, $params);	
-	}
-	
-	public function beforeFilter() {
-		parent::beforeFilter();
-		
 		//Actions which don't require authorization
-		$this->Auth->allow('*');
+		parent::beforeFilter();
+		$this->Auth->allow('display');
 	}
-	
+
+	public function getData($controller, $params){
+		
+		//set page title
+		$controller->set('title_for_layout', __('Gästebuch'));
+		
+		//load the used model in order to receive data
+		$controller->loadModel('Guestbook.GuestbookPost');
+		
+		//change default limit of items per page for paginator to 10
+		$this->Paginator->settings = array(
+		 			'limit' => 10,
+		);
+
+		//check user authorisation and get data
+		// TODO implement check
+		//normal user / guest is only allowed to see released and not deleted posts
+		// 		$allGuestbookPosts = $controller->paginate('GuestbookPost', array('released NOT' => NULL,'deleted' => NULL));
+		//admin user has to have unreleaased posts in order to release them
+		$allGuestbookPosts = $controller->paginate('GuestbookPost',array('deleted' => NULL));
+		return $allGuestbookPosts;
+	}
+
 }
