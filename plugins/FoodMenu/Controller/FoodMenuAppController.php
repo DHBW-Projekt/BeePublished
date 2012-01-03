4187e648c7ee13c1f59715d88c7c2d8d87430378
Benedikt Steffan <b.steffan@uni.de>
2012-01-03 11:20:29 +0100

    updated FoodMenuAppController

--- plugins/FoodMenu/Controller/FoodMenuAppController.php
+++ plugins/FoodMenu/Controller/FoodMenuAppController.php
@@ -2,7 +2,14 @@
 
 class FoodMenuAppController extends AppController {
 	var $helpers = array('Html', 'Form', 'Javascript', 'Ajax');
+	
+	function beforeFilter()
+    {
+        parent::beforeFilter();
 
+        //Actions which don't require authorization
+        $this->Auth->allow('*');
+    }
 
 	function viewMenu() {
 		$this->loadModel('FoodMenuMenu');
@@ -10,10 +17,6 @@ class FoodMenuAppController extends AppController {
 		$this->loadModel('FoodMenuEntry');
 		$this->FoodMenuMenu->bindModel(array('hasAndBelongsToMany' => array('FoodMenuCategory' => array('conditions'=>array('Category.ID'=>'*')))));
 
-		
-//		$menu = $this->FoodMenuMenu->find('all');
-//		$category = $this->FoodMenuCategory->find('all');
-//		$entries = $this->FoodMenuEntry->find('all');
 		$menu = $this->FoodMenuMenu->find('all');
 		$category = $this->FoodMenuCategory->find('all');
 		$entries = $this->FoodMenuEntry->find('all');
@@ -23,14 +26,30 @@ class FoodMenuAppController extends AppController {
 	}
 	
 	function showCategories() {
+		
+		$selectedMenu = $_POST[]; //Get ID of selected FoodMenu
+		
 		$this->loadModel('FoodMenuCategory');
 		$this->loadModel('FoodMenuMenu');
 		$this->FoodMenuMenu->bindModel(array('hasAndBelongsToMany' => array('FoodMenuCategory' => array())));
 
-		$menu = $this->FoodMenuMenu->find('all');
+		$menu = $this->FoodMenuMenu->find('all', array('conditions'=>array('Menu.name'=>$selectedMenu)));
+		
+		$this->render('/Elements/MenuLinks');
+	}
+	
+	function showEntries() {
+		
+		$selectedCategory = $_POST[]; //Get ID of selected FoodMenu
+		
+		$this->loadModel('FoodMenuCategory');
+		$this->loadModel('FoodMenuEntry');
+		
+		$this->FoodMenuCategory->bindModel(array('hasAndBelongsToMany' => array('FoodMenuEntry' => array())));
+
+		$entry = $this->FoodMenuEntry->find('all', array('conditions'=>array('Category.name'=>$selectedCategory)));
 		
 		$this->render('/Elements/MenuLinks');
 	}
 
 }
-
