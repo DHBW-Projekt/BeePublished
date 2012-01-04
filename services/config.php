f1a8ed31f8c9cf5347a81293bf732157bab53189
Alex <alexan.chr.mueller@googlemail.com>
2012-01-04 17:53:12 +0100

    onssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- services/config.php
+++
@@ -1,5 +0,0 @@
-HOMEURL#www.ms-mediagroup.de/Dualon
-DBHOST#mysql5.concept2designs.de
-DBNAME#db115933_10
-DBLOGIN#db115933_10
-DBPW#cms1
--- services/installation/index.php
+++ services/installation/index.php
@@ -2,8 +2,8 @@
 header('Content-Type: text/xml');
 include '../xmlbuilder.php';
 
-if(isset($_POST['DBHost']) && isset($_POST['DBName']) && isset($_POST['DBPw']) && isset($_POST['DBLogin'])){
-	if(createConfigFile($_POST['DBHost'], $_POST['DBLogin'], $_POST['DBName'], $_POST['DBPw'])) {
+if(isset($_POST['HomeUrl']) &&isset($_POST['DBHost']) && isset($_POST['DBName']) && isset($_POST['DBPw']) && isset($_POST['DBLogin'])){
+	if(createConfigFile($_POST['HomeUrl'],$_POST['DBHost'], $_POST['DBLogin'], $_POST['DBName'], $_POST['DBPw'])) {
 		//build up good answer xml
 		//start sql dump script
 		
@@ -14,27 +14,28 @@ if(isset($_POST['DBHost']) && isset($_POST['DBName']) && isset($_POST['DBPw']) &
 		$db_pw = $_POST['DBPw'];
 		$db_login = $_POST['DBLogin'];
 		
-		
+
 
 		//replace folder separators
 		$mysql_path = str_replace("\\","/",$mysql_path);
-		$db_script_path = str_replace("\\","/",$db_script_path);
-	
-		//build up script line
-		$mysqlDump = $mysql_path.'/mysql ';
-    	$mysqlDump .= '--host="' . $db_server . '" ';
-    	$mysqlDump .= '--user="' . $db_login . '" ';
-   		$mysqlDump .= '--password="' .$db_pw . '" ';
-   		$mysqlDump .= $db_name . ' < ' . $db_script_path;
-   		
-
-    	$returncodeexec = passthru($mysqlDump);
-
-
-		//go on with controlling whether the tables were created 
+		$db_script_path = str_replace("\\","/",$db_script_path);	
 		
+
+		
+		
+		//build up script line
+		$mysqlDump = 'mysql ';
+    	$mysqlDump .= '-h ' . $db_server . ' ';
+    	$mysqlDump .= '-u ' . $db_login . ' ';
+   		$mysqlDump .= '-p' .$db_pw . ' ';
+   		$mysqlDump .= $db_name . ' < ' . 'cake.sql';
+		
+    	$returncodeexec = passthru($mysqlDump);
+		//passthru("mysql -h mysql5.ms-mediagroup.de -u db115933_10 -pcms1 db115933_10 < "."cake.sql");
+
     	//step 1: connect to db
-		@$dbhost = mysql_connect($db_server, $db_login, $db_pw) OR die("Alex");
+
+		@$dbhost = mysql_connect($db_server, $db_login, $db_pw) OR die("Alexblupp");
 	
 		@$db = mysql_select_db($db_name,$dbhost) OR die("Alex");
     	//step 2:build up query string
@@ -49,21 +50,25 @@ if(isset($_POST['DBHost']) && isset($_POST['DBName']) && isset($_POST['DBPw']) &
 		$numberoftables = $row[0];
 		//step4: build up answer
 		if($numberoftables == 16){
-			echo getRC0Xml();
+			echo getAnswer( getRC0Xml());
+			
 		} else {
-			echo getRC1Xml();
+			echo "test2";
+			echo getAnswer( getRC1Xml());
 		}
  	
 	} else {
-		echo getRC1Xml();
+		echo "test";
+		echo getAnswer( getRC1Xml());
 		//build up bad answer xml
 	}
 }else {
-		echo getRC1Xml();
+		echo "test";
+		echo getAnswer( getRC1Xml());
 }
 
-function createConfigFile($dbhost,$dblogin, $dbname, $dbpw){
-	//buildup string for File
+function createConfigFile($homeurl, $dbhost,$dblogin, $dbname, $dbpw){
+	//buildup string for cake File
 
 	$file = "<?php class DATABASE_CONFIG { 
 public \$default = array(
@@ -85,9 +90,34 @@ public \$default = array(
 	//put data into file
 	fputs($dbfile, $file);
 	
-	fclose($dbfile);
+	$cake_configtbool = fclose($dbfile);
 	
-	return true;
+	
+	//start building up simple config file 
+	
+	$c_filename = "../config.php";
+	$c_file = fopen($c_filename, "w+");
+	rewind($c_file);
+	//write rootdir
+	$to_write = "HOMEURL#".$homeurl."\n";
+	fwrite($c_file, $to_write);
+	//write dbhost
+	$to_write = "DBHOST#".$dbhost."\n";
+	fwrite($c_file, $to_write);
+	//write dbname
+	$to_write = "DBNAME#".$dbname."\n";
+	fwrite($c_file, $to_write);
+	//write dbuser
+	$to_write = "DBLOGIN#".$dblogin."\n";
+	fwrite($c_file, $to_write);
+	//write dbpasswort
+	$to_write = "DBPW#".$dbpw."";
+	fwrite($c_file, $to_write);
+	
+	
+	$c_configtbool = fclose($c_file);
+	
+	return ($cake_configtbool && $c_configtbool );
 }
 
 
