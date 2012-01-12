97418f910220d31dd925d64241509eb67e20a6f2
Alex <alexan.chr.mueller@googlemail.com>
2012-01-12 14:34:45 +0100

    Basics gallery

---
+++ plugins/Gallery/Controller/GalleryController.php
@@ -0,0 +1,13 @@
+<?php
+App::uses('GalleryAppController', 'Gallery.Controller');
+
+class GalleryController extends GalleryAppController {
+	
+	public $components = array('ContentValueManager');
+
+    public function admin($contentId)
+    {
+        $this->layout = 'overlay';
+        
+    }
+}
