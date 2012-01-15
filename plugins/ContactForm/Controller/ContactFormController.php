e84004c44e61f5a51a0e54782e80e170793ab278
Max <maximilian.stueber@me.com>
2012-01-16 00:10:48 +0100

    Some cleaning up ...

--- plugins/ContactForm/Controller/ContactFormController.php
+++ plugins/ContactForm/Controller/ContactFormController.php
@@ -8,37 +8,10 @@ App::uses('CakeEmail', 'Network/Email');
  */
 class ContactFormController extends AppController {
 	
-	//SET name
-	public $name = 'ContactForm';
-	
-	//LOAD helpers
-	//public $helpers = array ('Recaptcha'); /*'Html','Form',*/
-	
-	//LOAD components
-	//var $components = array('Recaptcha');  /*'ContactFormComponent'*/
-	
-	//LOAD elements
-	//var $uses = array('ContactForm','MailAddress');
-	
-	/*public function saveaddress(){
-		if ($this->MailAddress->validates()) {
-			if ($this->MailAddress->save($this->data)) {
-				//soll mailaddress-Wert in Tabelle �berspeichern -> wie?
-				$this->Session->setFlash('The e-mail address has been saved.');
-			}else{
-				$this->Session->setFlash('An error occured. The e-mail address could not be saved.');
-			}
-		}
-	}*/
-	
    /**
 	* Before-Filter function
 	*/
 	function beforeFilter(){
-		//Recaptcha
-    	$this->Recaptcha->publickey = "6LeXXMwSAAAAAATYW9zan7IB7yaIKmx1VPMjqeXX";
-    	$this->Recaptcha->privatekey = "6LeXXMwSAAAAALTEji2U_qC9lp4W38_QxC0zfhgX";
-    	
     	//Permissions
     	$this->Auth->allow('*');
 	}	
