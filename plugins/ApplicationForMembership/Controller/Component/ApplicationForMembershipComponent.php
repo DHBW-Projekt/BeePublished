3a4d15d369b41f0df06d0a2913bf6dc242b46be5
Max <maximilian.stueber@me.com>
2012-01-16 18:46:21 +0100

    some more changes ...

--- plugins/ApplicationForMembership/Controller/Component/ApplicationForMembershipComponent.php
+++ plugins/ApplicationForMembership/Controller/Component/ApplicationForMembershipComponent.php
@@ -3,7 +3,7 @@
 class ApplicationForMembershipComponent extends Component {
 	
 	public $uses = array('Sanitize');
-	public $components = array('PermissionValidation');
+	public $components = array('BeeEmail', 'PermissionValidation');
 	
 	
    /**
@@ -40,63 +40,54 @@ class ApplicationForMembershipComponent extends Component {
 	/**
 	 * Function send.
 	 */
-	public function send(){
-	
-		//CHECK request
-		if ($this->request->is('post')){
-				
-			//Attribute
-			$form_error = false;
-				
-			//LOAD model
-			$this->loadModel('ApplicationForMembership');
-				
-			//GET data
-			$data = $this->request->data;
-				
-			//VALIDATES user input
-			if(!$this->ApplicationForMembership->validates()){
-				$form_error = true;
-	
-				//some error handling should be here ....
-			}
-				
-				
-			//DELETE code injections
-			if(!$form_error){
-				App::uses('Sanitize','Utility');
-				$data = Sanitize::clean($data);
-			}
-			//CREATE mail
-			if(!$form_error){
-				//BUILD mail
-				App::uses('CakeEmail', 'Network/Email');
-				//CakeEmail::deliver('you@example.com', 'Subject', 'Message', array('from' => 'me@example.com'));
-	
-				//$email = new CakeEmail();
-				//$email->template('ApplicationForMembership.application', 'email');
-				//$email->emailFormat('text');
-				//$email->to('yvonnelaier@gmx.de');
-				//$email->from('noreply@dualoncms.de');
-				//$email->subject('New Application for Membership');
-				 
-				//$email->send('Testinhalt');
-				//->viewVars(array('data' => $data,'url' => 'localhost'))
-			}
-				
-			//SAVE in db
-			/*if ($controller->ApplicationForMembership->save($controller->request->data)) {
-			 $controller->Session->delete('input');
-			$controller->_deleteValidation();
-			$controller->Session->setFlash(__('Your application for membership was sent. You are going to be contacted by a responsible person.'), 'default', array('class' => 'flash_success'), 'ApplicationForMembership');
-			$controller->redirect($controller->referer());
-			}
-			$this->Session->setFlash(__('An error has occured.'), 'default', array('class' => 'flash_failure'), 'ApplicationForMembership');
-			$this->_persistValidation('ApplicationForMembership');
-			$this->Session->write('input', $this->request->data);*/
-				
-			//REDIRECT
-			$this->redirect($this->referer());
+	public function send($controller, $url=null){
+		
+		//Attributes
+		$data_error = false;
+		 
+		//LOAD model
+		$controller->loadModel("ApplicationForMembership");
+		
+		//CHECK request and data
+		if (!$controller->request->is('post') || !isset($controller->data['ApplicationForMembership']))
+			$data_error = true;
+		 
+		//CHECK recaptcha
+		/*if(!$this->Recaptcha->valid($controller->params['form'])){
+		 $data_error = true;
+		}*/
+		
+		//VALIDATE data
+		if(!$data_error){
+			$controller->ApplicationForMembership->set($controller->data['ApplicationForMembership']);
+		
+			if(!$controller->ContactRequest->validates())
+				$data_error = true;
 		}
+		
+		//App::uses('Sanitize','Utility');
+		//$data = Sanitize::clean($data);
+		
+		//SEND email
+		if(!$data_error){
+			$this->BeeEmail->sendHtmlEmail($to = 'maximilian.stueber@me.com',
+			$subject = 'New Application for Membership',
+			$viewVars = array('data' => $controller->data['ApplicationForMembership'], 'url' => 'localhost'),
+			$viewName = 'ApplicationForMembership.application');
+		}
+		 
+		//SAVE in db
+		if(!$data_error){
+			$data_error = !$controller->ApplicationForMembership->save($controller->data['ApplicationForMembership']);
+		}
+		
+		//PRINT error messages
+		if(!$data_error)
+			$controller->Session->setFlash('Thank you for your interest. Your request has been sent.');
+		else
+			$controller->Session->setFlash('Please fill out all mandatory fields.');
+		 
+		//REDIRECT
+		$controller->redirect('/application_for_membership');
 	}
 }
