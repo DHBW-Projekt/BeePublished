71e5e92936df3b740eaaa6f28011f34c2f34168d
Tobias Hoehmann <tobiashoehmann@googlemail.com>
2012-01-17 02:29:36 +0100

    Added creation and activation of Email Templates

---
+++ app/Model/EmailTemplate.php
@@ -0,0 +1,45 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * EmailTemplate Model
+ *
+ */
+class EmailTemplate extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'This field cannot be empty.'
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'isunique' => array(
+            	'rule'    => 'isUnique',
+            	'message' => 'The name already exists.'
+			),
+		),
+		'active' => array(
+			'boolean' => array(
+				'rule' => array('boolean'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
