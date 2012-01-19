332d5436cc41422e8d5437496fcb1c182d254a10
Viene S <j.dachs64@googlemail.com>
2012-01-19 13:37:52 +0100

    mulilanguage for static text

--- plugins/StaticText/Controller/DisplayTextController.php
+++ plugins/StaticText/Controller/DisplayTextController.php
@@ -21,14 +21,14 @@ class DisplayTextController extends StaticTextAppController {
 			//save data
 			if ($this->request->is('post')) {
 				$this->ContentValueManager->saveContentValues($contentId, $this->request->data['null']);
-				$this->Session->setFlash('Successfully saved');
+				$this->Session->setFlash(__d('static_text', 'Successfully saved'));
 			}
 			//load data with contentId
 			$contentValues = $this->ContentValueManager->getContentValues($contentId);
 			if (array_key_exists('Text', $contentValues)) {
 				$text = $contentValues['Text'];
 			} else {
-				$text = "Leer"; //"Leer" or '' ?
+				$text = __d('static_text',"empty"); //"Leer" or '' ?
 			} 
 			
 			if (array_key_exists('Published', $contentValues)) {
@@ -45,7 +45,7 @@ class DisplayTextController extends StaticTextAppController {
 				);
 			}
 		} else    { //If you are not aloowed to
-		   $this->Session->setFlash(__('You are not authenticated to enter these page!'));
+		   $this->Session->setFlash(__d('static_text','You are not authenticated to enter these page!'));
 		   //Go to mainpage
 			$this->redirect($this->referer());
 		}
