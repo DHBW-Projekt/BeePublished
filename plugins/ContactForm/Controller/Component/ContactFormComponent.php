19b0ed9f4add5dc4d19446d23171b1dbe71563a4
AnniRoc <corinna.knick@yahoo.de>
2012-01-19 09:19:54 +0100

    some changes

--- plugins/ContactForm/Controller/Component/ContactFormComponent.php
+++ plugins/ContactForm/Controller/Component/ContactFormComponent.php
@@ -3,115 +3,143 @@
 App::import('Vendor','recaptcha/recaptchalib');
 
 class ContactFormComponent extends Component {
-	
+
 	//SET name
-    public $name = 'ContactFormComponent';
-    
-    //Component
-    var $components = array('BeeEmail');
+	public $name = 'ContactFormComponent';
 
-   /**
-    * Method to transfer data from plugin to CMS.
-    */
-    public function getData($controller, $params, $url, $contentId, $myUrl)
-    {
-    	//SET title
-    	$controller->set('title_for_layout', __('Contact Form'));
-    	
-    	//CHECK url
-    	if (isset($url)){
-    		$data['Element'] = array_shift($url);
-    	} else {
-    		$data['Element'] = 'request';
-    	}
-    
-    	//CALL corresponding comp. method
-    	if (method_exists($this, $data['Element'])){
-    		$func_data = $this->{$data['Element']}($controller, $url, $params, $myUrl);
-    		if (isset($func_data['data'])) {
-    			$data['data'] = $func_data['data'];
-    		}
-    		if (isset($func_data['Element'])) {
-    			$data['Element'] = $func_data['Element'];
-    		}
-    	}
-    
-    	//RETURN data
-    	if (!isset($data['data'])) {
-    		$data['data'] = null;
-    	}
-    		
-    	return $data;
-    }
-    
-   /**
-    * Function send.
-    */
-    public function send($controller, $url=null) {
-    	
-    	//Attributes
-    	$data_error = false;
-    	
-    	//LOAD model
-    	$controller->loadModel("ContactForm.ContactRequest");
-    	
-    	//CHECK request and data
-    	if (!$controller->request->is('post') || !isset($controller->data['ContactForm']))
-    		$data_error = true;
-    	
+	//Component
+	var $components = array('BeeEmail', 'Config');
 
-    	//VALIDATE data
-    	if(!$data_error){
-    		$controller->ContactRequest->set(array('name' => $controller->data['ContactForm']['name'],
-    											   'email' => $controller->data['ContactForm']['email'],
-    											   'subject' => $controller->data['ContactForm']['subject'],
-    											   'body' => $controller->data['ContactForm']['body']
-    										
-    		));
+	/**
+	 * Method to transfer data from plugin to CMS.
+	 */
+	public function getData($controller, $params, $url, $contentId, $myUrl)
+	{
+		//SET title
+		$controller->set('title_for_layout', __('Contact Form'));
+			
+		//CHECK url
+		if (isset($url)){
+			$data['Element'] = array_shift($url);
+		} else {
+			$data['Element'] = 'request';
+		}
 
-    		if(!$controller->ContactRequest->validates())
-    			$data_error = true;
-    	}
-    	
-    	//VALIDATE captcha
-    	if(!$data_error){
-	    	
-    		$privatekey = "6LfzYcwSAAAAAEH-Nr-u6qaFaNdIc6h9nlbm0i76";
-	    	
-	    	$resp = recaptcha_check_answer($privatekey,
-	    								   $_SERVER["REMOTE_ADDR"],
-									       $controller->data['recaptcha_challenge_field'],
-									       $controller->data['recaptcha_response_field']
-	    	);
-	    	
-	    	if ($resp->is_valid) {
-	    		$data_error = true;
-	    	}
-    	}
+		//CALL corresponding comp. method
+		if (method_exists($this, $data['Element'])){
+			$func_data = $this->{$data['Element']}($controller, $url, $params, $myUrl);
+			if (isset($func_data['data'])) {
+				$data['data'] = $func_data['data'];
+			}
+			if (isset($func_data['Element'])) {
+				$data['Element'] = $func_data['Element'];
+			}
+		}
 
-    	//SEND email
-    	if(!$data_error){
-    		$this->BeeEmail->sendHtmlEmail($to = 'corinna.knick@yahoo.de',
-    									   $subject = 'Request through contact form',
-    									   $viewVars = array('name' => $controller->data['ContactForm']['name'],
-																	   'email' => $controller->data['ContactForm']['email'],
-																	   'subject' => $controller->data['ContactForm']['subject'],
-																	   'body' => $controller->data['ContactForm']['body']
-														     ),
-										   $viewName = 'ContactForm.contact');
-    	}
-    	
-    	//PRINT error messages
-    	if(!$data_error)
-    		$controller->Session->setFlash('Thank you for your interest. Your request has been sent.');
-    	else
-    		$controller->Session->setFlash('Please fill out all mandatory fields.');
-    	
-    	//RETURN data
-    	if($data_error)
-    		return array('data' => $controller->ContactRequest, 'Element' => 'request');
-    	
-    	//REDIRECT
-    	$controller->redirect(/*$url.*/'/contact-form');
-    } 
-} 
+		//RETURN data
+		if (!isset($data['data'])) {
+			$data['data'] = null;
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Function send.
+	 */
+	public function send($controller, $url=null) {
+			
+		//Attributes
+		$data_error = false;
+		$captcha_error = false;
+		$mail_error = false;
+		$input_error = false;
+			
+		//LOAD model
+		$controller->loadModel("ContactForm.ContactRequest");
+			
+		//CHECK request and data
+		if (!$controller->request->is('post') || !isset($controller->data['ContactForm'])){
+			$data_error = true;
+		}
+			
+
+		if(!$data_error){
+				
+			//VALIDATE data
+			$controller->ContactRequest->set(array('name' => $controller->data['ContactForm']['name'],
+													'email' => $controller->data['ContactForm']['email'],
+													'subject' => $controller->data['ContactForm']['subject'],
+													'body' => $controller->data['ContactForm']['body']
+
+			));
+
+			if($controller->ContactRequest->validates()){
+			
+				//VALIDATE captcha
+				$privatekey = "6LfzYcwSAAAAAEH-Nr-u6qaFaNdIc6h9nlbm0i76";
+				$resp = recaptcha_check_answer($privatekey,
+				$_SERVER["REMOTE_ADDR"],
+				$controller->data['recaptcha_challenge_field'],
+				$controller->data['recaptcha_response_field']
+				);
+				
+				if($resp->is_valid){
+					
+					//CHECK recipient
+					$mailaddress = $this->Config->getValue('email');
+					if (isset ($mailaddress)){
+						
+						//SEND email
+						if($this->BeeEmail->sendHtmlEmail($to = $mailaddress, $subject = 'Request through contact form', $viewVars = array(
+																								'name' => $controller->data['ContactForm']['name'],
+																							   'email' => $controller->data['ContactForm']['email'],
+																							   'subject' => $controller->data['ContactForm']['subject'],
+																							   'body' => $controller->data['ContactForm']['body']), 
+															$viewName = 'ContactForm.contact')){
+							$controller->Session->setFlash('Thank you for your interest. Your request has been sent.');
+						}else{
+							$mail_error = true;
+						} //END send email
+															
+					}else{
+						$mail_error = true;
+					} //END check recipient
+					
+				}else{
+					$captcha_error = true;
+				} //END if captcha validates
+
+			}else{
+				$input_error = true;
+			} //END if data validates
+		
+		} //END if data error
+
+			
+		//PRINT error messages
+		if($data_error){
+			$controller->Session->setFlash('An Error has occured.Please try again.');
+		}
+		
+		if($input_error){
+			$controller->Session->setFlash('Please fill out all mandatory fields correctly.');
+		}
+		
+		if($captcha_error){
+			$controller->Session->setFlash('Please fill out the CAPTCHA field.');
+		}
+		
+		if($mail_error){
+			$controller->Session->setFlash('An error occured, your request could not be sent. Please contact an administrator.');
+		}
+			
+		//RETURN data
+		if($data_error || $input_error || $captcha_error || $mail_error){
+			return array('data' => $controller->ContactRequest, 'Element' => 'request');
+		}
+			
+		//REDIRECT
+		$controller->redirect(/*$url.*/'/contact-form');
+	}
+}
--- plugins/ContactForm/View/Elements/request.ctp
+++ plugins/ContactForm/View/Elements/request.ctp
@@ -3,37 +3,46 @@
 	//CAPTCHA
 	App::import('Vendor','recaptcha/recaptchalib');
 	$publickey = "6LfzYcwSAAAAAN3vRDzZKXkC0rYkwaKQTi8hMkj6";
+	//echo $this->Html->css('/ContactForm/css/contactform');
 ?>
 
-<div id='contactform_form'>
+<form id = 'contactform_form'>
 	<h2>Contact Form</h2>
-	<?php 
-		$validationError = $this->Session->read('Validation.ContactForm.validationErrors');
-		echo $this->Session->flash('ContactForm');
-
-		echo $this->Form->create('ContactForm', array('url' => $url.'/contactform/send'));
-	?>
-	<table>
-		<tr>
-			<td><?php echo $this->Form->label('name', __('Name*: '));?></td>
-			<td><?php echo $this->Form->input('name', array('label' => ''));?></td>
-		</tr>
-		<tr>
-			<td><?php echo $this->Form->label('email', __('E-Mail*: '));?></td>
-			<td><?php echo $this->Form->input('email', array('label' => ''));?></td>
-		</tr>
-		<tr>
-			<td><?php echo $this->Form->label('subject', __('Subject*: '));?></td>
-			<td><?php echo $this->Form->input('subject', array('label' => ''));?></td>
-		</tr>
-		<tr>
-			<td><?php echo $this->Form->label('body', __('Message*: '));?></td>
-			<td><?php echo $this->Form->input('body', array('label' => '', 'rows' => '4'));?></td>
-		</tr>
-		<tr>
-			<td colspan="2"><?php echo recaptcha_get_html($publickey);?></td>
-		</tr>
-	</table>
-	<?php echo $this->Form->end('Send');?>
-	</div>
-</div>
+		<?php 
+			$validationError = $this->Session->read('Validation.ContactForm.validationErrors');
+			echo $this->Session->flash('ContactForm');
+	
+			echo $this->Form->create('ContactForm', array('url' => $url.'/contactform/send'));
+		?>
+		<div id = 'contactform_div'>
+		<table>
+			<tr>
+				<td><?php echo $this->Form->label('name', __('Name: '));?></td>
+				<td><?php echo $this->Form->input('name', array('label' => '', 'type' => 'text', 'maxlength' => '30'));?></td>
+			</tr>	
+			<tr>
+				<td><?php echo $this->Form->label('email', __('E-Mail*: '));?></td>
+				<td><?php echo $this->Form->input('email', array('label' => '', 'maxlength' => '40'));?></td>
+			</tr>
+			<tr>
+				<td><?php echo $this->Form->label('subject', __('Subject*: '));?></td>
+				<td><?php echo $this->Form->input('subject', array('label' => ''));?></td>
+			</tr>
+			<tr>
+				<td><?php echo $this->Form->label('body', __('Message*: '));?></td>
+				<td><?php echo $this->Form->input('body', array('label' => '', 'rows' => '4', 'type' => 'text'));?></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>* - Mandatory field</td>
+			</tr>
+		</table><br>
+		</div>
+		
+		<div id='contactform_bottom'>
+		
+			<?php echo recaptcha_get_html($publickey);?><br>
+			
+			<?php echo $this->Form->end('Send');?><br>
+		</div>
+</form>
