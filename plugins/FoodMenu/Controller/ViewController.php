f0bbdeb9d0f801b5d379a43340bcef1106ec77f7
Benedikt Steffan <b.steffan@uni.de>
2012-01-19 16:46:56 +0100

    deleted a unused method in ViewController

--- plugins/FoodMenu/Controller/ViewController.php
+++ plugins/FoodMenu/Controller/ViewController.php
@@ -22,36 +22,6 @@ class ViewController extends FoodMenuAppController {
 		$this->render('/FoodMenuMenus/index');
 	}
 	
-//	 function addCategoriesToMenu() {
-//		// Has any form data been POSTed?
-//    	if ($this->request->is('post')) {
-//        	// If the form data can be validated and saved...
-//        	$save = $this->request->data;
-//        	if ($this->FoodMenuCategory->saveAll($save)) {
-//            	// Set a session flash message and redirect.
-//            	$this->Session->setFlash("Kategorien hinzugefügt");
-//            	$this->set('mode', null);
-//            	$this->redirect($this->referer());
-//            	//$this->render('/View/admin');
-//        	}
-//    	}
-//	    // If no form data, find the recipe to be edited
-//    	// and hand it to the view.
-//    	$menu = $this->FoodMenuMenu->findById($id);
-//    	$this->set('menu', $menu);
-//    	$this->set('mode', 'add');
-//    	
-//    	//Submit variables of admin method to make back-button work
-//    	$menus = $this->FoodMenuMenu->find('all');
-//		$this->set('menus', $menus);
-//		$categories = $this->FoodMenuCategory->find('all');
-//		$this->set('categories', $categories);
-//		$entries = $this->FoodMenuEntry->find('all');
-//		$this->set('entries', $entries);
-//		
-//    	$this->render('/FoodMenuMenu/index');
-//	}
-	
 	function selectDate() {
 		if ($this->request->is('post') || $this->request->is('put')) {
 			$data = $this->request->data;
