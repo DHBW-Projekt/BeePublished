cab646a732fe6f012f52e387ba1f8d0a07d0c456
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-02 14:05:31 +0100

    update

--- plugins/Gallery/Controller/GalleryOverviewController.php
+++ plugins/Gallery/Controller/GalleryOverviewController.php
@@ -1,6 +1,6 @@
 <?php
 class GalleryOverviewController extends AppController {
-	var $layout = 'overlay';
+	var $layout = 'overlay'; 
 	public function admin($contentId){
 		$this->set("ContentId",$contentId);
 		$this->set("mContext",'overview');		
