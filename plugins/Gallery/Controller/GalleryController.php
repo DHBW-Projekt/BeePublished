d2ed6f309142e4a51de286a3554e5e59e003b12d
Alexander Mueller <Alex@Alex-PC.(none)>
2012-02-02 22:41:27 +0100

    Added Comments and disclaimer

--- plugins/Gallery/Controller/GalleryController.php
+++ plugins/Gallery/Controller/GalleryController.php
@@ -1,10 +1,34 @@
 <?php
+/*
+ * This file is part of BeePublished which is based on CakePHP.
+ * BeePublished is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or any later version.
+ * BeePublished is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public
+ * License along with BeePublished. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * @copyright 2012 Duale Hochschule Baden-Württemberg Mannheim
+ * @author Alexander Müller & Fabian Kajzar
+ * 
+ * 
+ */
+
 App::uses('GalleryAppController', 'Gallery.Controller');
 
 class GalleryController extends GalleryAppController {
 	
 	public $components = array('ContentValueManager');
 
+    /**
+     * is called when the admin menu is called
+     * @param unknown_type $contentId
+     */
     public function admin($contentId)
     {
         $this->layout = 'overlay';
--- plugins/Gallery/View/ManageGalleries/assign_images.ctp
+++ plugins/Gallery/View/ManageGalleries/assign_images.ctp
@@ -1,4 +1,25 @@
 <?php
+/*
+ * This file is part of BeePublished which is based on CakePHP.
+ * BeePublished is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or any later version.
+ * BeePublished is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public
+ * License along with BeePublished. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * @copyright 2012 Duale Hochschule Baden-Württemberg Mannheim
+ * @author Alexander Müller & Fabian Kajzar
+ * 
+ * @description view in admin area to assign images to a gallery
+ */
+
+
 $this->Html->script('/gallery/js/assign', false);
 $this->Html->css('/gallery/css/galleries', NULL, array('inline' => false));
 echo $this->element('admin_menu',array("ContentId" => $ContentId, "mContext" => $mContext));
