90b7069f0edd8fb29aa4c8372fa30828f1112cad
marcuslieberenz <marcuslieberenz@googlemail.com>
2012-02-08 18:51:43 +0100

    changed menu.js files

--- app/View/Themed/BeeColourful/webroot/js/menu.js
+++ app/View/Themed/BeeColourful/webroot/js/menu.js
@@ -9,4 +9,51 @@ function initMenu() {
             $(this).children("ol.subnav").hide();
         }
     );
+    var menuswitchelement = $(document.createElement("a")).attr({
+		"id" : "menuswitch",
+		"href" : "#",
+		"style" : "text-align:left;"
+	});
+	
+	if($("#menuswitch").length<1) {
+	$("#topnav-content").append(menuswitchelement);
+	}
+	var hidden = "false";
+	if (document.cookie) {
+		cookiefile = document.cookie;
+//		hidden = cookiefile.slice(cookiefile.indexOf("hidden=") + 7, cookiefile
+//				.indexOf("hidden=") + 12);
+		if (hidden == "false") {
+			$("#pagelogo").show();
+			$("#menu").show();
+			prepareforhide();
+		} else {
+			$("#pagelogo").hide();
+			$("#menu").hide();
+			prepareforshow();
+		}
+	} else {
+		document.cookie = "hidden=" + hidden;
+		prepareforhide();
+	}
+	function prepareforhide() {
+		$(menuswitchelement).html("Menu verstecken >>").click(function() {
+			$("#pagelogo").hide();
+			$("#menu").hide();
+			hidden = "true";
+			document.cookie = "hidden=" + hidden;
+			prepareforshow();
+		});
+
+	}
+	function prepareforshow() {
+		$(menuswitchelement).html("Menu anzeigen >>").click(function() {
+			$("#pagelogo").show();
+			$("#menu").show();
+			hidden = "false";
+			document.cookie = "hidden=" + hidden;
+			prepareforhide();
+		});
+
+	}
 }
