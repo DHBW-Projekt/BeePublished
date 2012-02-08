3853a803b31fa28d3d103a93887da543f4f3bf68
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-08 15:46:22 +0100

    delete picture

--- plugins/Gallery/Controller/Component/GalleryPictureCompComponent.php
+++ plugins/Gallery/Controller/Component/GalleryPictureCompComponent.php
@@ -30,14 +30,14 @@ class GalleryPictureCompComponent extends Component
 	 * @param unknown_type $controller
 	 * @param unknown_type $pictureId
 	 */
-	public function getPicture($controller, $pictureId){
+	public function getPicture($controller, $pictureId){	
 		$controller->loadModel('Gallery.GalleryPicture');
 		$picture = $controller->GalleryPicture->findById($pictureId);
-		
-		$picture = $this->normalizePicture($picture);
-		
-		return $picture;
-	}
+		if(!$picture)
+			return null;
+		else
+			return $this->normalizePicture($picture);
+		}
 	
 	/**
 	 * loads all pictures from db
@@ -102,29 +102,21 @@ class GalleryPictureCompComponent extends Component
 	 */
 	public function delete($controller, $pictureId){
 		$picture = $this->getPicture($controller, $pictureId);
-		
 		$pathInfo = pathinfo(realpath($picture['path_to_pic']));
-	
 		$thumbPath = $pathInfo['dirname']."/thumb/".$pathInfo['basename'];
-		
 		unlink($thumbPath);
-		
 		unlink(realpath($picture['path_to_pic']));
-		
 		$controller->loadModel('Gallery.GalleryPicture');
 		$pictures = $controller->GalleryPicture->delete($picture);
-		
-		
-		
 	}
 	
 	/**
 	 * normalizes the picture output -> add thumb path
 	 * @param unknown_type $picture
 	 */
-	public function normalizePicture($picture){
+	public function normalizePicture($picture){		
 		if(isset($picture['GalleryPicture']))
-		$picture = $picture['GalleryPicture'];
+			$picture = $picture['GalleryPicture'];
 		
 		$pathInfo = pathinfo($picture['path_to_pic']);	
 		
